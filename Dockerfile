bb236956de4455b17b3a6a13e641354db94f779c
Jonas Rubeck <jaons.rubeck@qaware.de>
2020-07-21 13:27:18 +0200

    add cloudbuild.yaml and Dockerfile, initial Web-UI

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:10.15.3 as source
+WORKDIR /src/web-ui
+COPY package.json ./
+RUN npm install
+COPY . ./
+RUN npm run build
+
+
+FROM nginx:1.19.0-alpine
+ENV PORT 8080
+ENV HOST 0.0.0.0
+WORKDIR /opt/web-ui
+COPY --from=source /src/build-your-own-radar/dist .
+COPY default.template /etc/nginx/conf.d/default.conf
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <PackChart :tweetData=loadData />
+  </div>
+</template>
+
+<script>
+  import * as d3 from "d3";
+
+  import PackChart from "./components/Chart.vue";
+
+  export default {
+    name: "app",
+    components: {
+      PackChart
+    },
+    data: function() {
+      return {
+        loadData: []
+      };
+    },
+    mounted() {
+      console.log("App loaded");
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+        let data = await d3.json("./tweets.json");
+        this.loadData = data;
+      }
+    }
+  };
+</script>
+
+<style>
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
