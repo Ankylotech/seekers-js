d74593943841e3b3c32719b38474a5b416b267a5
Jonas Rubeck <jaons.rubeck@qaware.de>
2020-07-22 17:34:05 +0200

    functioning Timelines

--- src/components/Timeline/DeviceTimeline.vue
+++ src/components/Timeline/DeviceTimeline.vue
@@ -2,6 +2,11 @@
     <div>
         <div id="timeline" v-if="hasLoaded">
             <h2> {{device}} </h2>
+            <span v-for="value in values" :key="value">
+                <input checked type="checkbox" :id="value" :value="value" v-model="showTimeline" >
+                <label :for="value">{{value}}</label>
+            </span>
+            <br>
             <svg  :id="device">
 
             </svg>
@@ -17,7 +22,9 @@
         data() {
             return {
                 data: {},
-                hasLoaded: true
+                hasLoaded: true,
+                showTimeline: [],
+                values: []
             };
         },
         props: {
@@ -25,57 +32,62 @@
             device: String,
             colors: Object
         },
+        updated() {
+            this.getChart();
+        },
         methods: {
             getChart: function () {
                 const selector = "#" + this.device;
                 let svg = d3.select(selector);
-                svg.empty();
+                svg.selectAll('*').remove();
                 Object.keys(this.data).forEach((key) => {
-                    let data = this.data[key];
-                    const margin = ({top: 20, right: 40, bottom: 30, left: 40});
-                    const height = 120;
-                    const width = 400;
-                    svg.attr("viewBox", [0, 0, width, height]);
-                    let yAxis = g => g
-                        .attr("transform", `translate(${margin.left},0)`)
-                        .call(d3.axisLeft(y))
-                        .call(g => g.select(".domain").remove())
-                        .call(g => g.select(".tick:last-of-type text").clone()
-                            .attr("x", 3)
-                            .attr("text-anchor", "start")
-                            .attr("font-weight", "bold")
-                            .text(data.y))
-                    let y = d3.scaleLinear()
-                        .domain([0, d3.max(data, d => d.value)]).nice()
-                        .range([height - margin.bottom, margin.top])
-                    let xAxis = g => g
-                        .attr("transform", `translate(0,${height - margin.bottom})`)
-                        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
-                    let x = d3.scaleUtc()
-                        .domain(d3.extent(data, d => d.date))
-                        .range([margin.left, width - margin.right])
-                    let line = d3.line()
-                        .defined(d => !isNaN(d.value))
-                        .x(d => x(d.date))
-                        .y(d => y(d.value))
+                    if(this.showTimeline.includes(key)) {
+                        let data = this.data[key];
+                        const margin = ({top: 20, right: 40, bottom: 30, left: 40});
+                        const height = 120;
+                        const width = 400;
+                        svg.attr("viewBox", [0, 0, width, height]);
+                        let yAxis = g => g
+                            .attr("transform", `translate(${margin.left},0)`)
+                            .call(d3.axisLeft(y).tickValues([]))
+                            .call(g => g.select(".domain").remove())
+                            .call(g => g.select(".tick:last-of-type text").clone()
+                                .attr("x", 3)
+                                .attr("text-anchor", "start")
+                                .attr("font-weight", "bold")
+                                .text(data.y))
+                        let y = d3.scaleLinear()
+                            .domain([0, d3.max(data, d => d.value)]).nice()
+                            .range([height - margin.bottom, margin.top])
+                        let xAxis = g => g
+                            .attr("transform", `translate(0,${height - margin.bottom})`)
+                            .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+                        let x = d3.scaleUtc()
+                            .domain(d3.extent(data, d => d.date))
+                            .range([margin.left, width - margin.right])
+                        let line = d3.line()
+                            .defined(d => !isNaN(d.value))
+                            .x(d => x(d.date))
+                            .y(d => y(d.value))
 
-                    svg.append("g")
-                        .call(xAxis);
+                        svg.append("g")
+                            .call(xAxis);
 
-                    svg.append("g")
-                        .call(yAxis);
-                    let color = this.colors[key];
-                    console.log(key + "-" + color)
-                    if(!color) color = "steelblue";
+                        svg.append("g")
+                            .call(yAxis);
+                        let color = this.colors[key];
+                        console.log(key + "-" + color)
+                        if (!color) color = "steelblue";
 
-                    svg.append("path")
-                        .datum(data)
-                        .attr("fill", "none")
-                        .attr("stroke", color)
-                        .attr("stroke-width", 0.5)
-                        .attr("stroke-linejoin", "round")
-                        .attr("stroke-linecap", "round")
-                        .attr("d", line);
+                        svg.append("path")
+                            .datum(data)
+                            .attr("fill", "none")
+                            .attr("stroke", color)
+                            .attr("stroke-width", 0.75)
+                            .attr("stroke-linejoin", "round")
+                            .attr("stroke-linecap", "round")
+                            .attr("d", line);
+                    }
                 })
             },
             getData: async function(application = this.application){
@@ -86,7 +98,9 @@
                             let date = datapoint.uploadedAt._seconds;
                             delete datapoint.uploadedAt;
                             Object.keys(datapoint).forEach((key) => {
-
+                                if(!this.values.includes(key)) {
+                                    this.values.push(key);
+                                }
                                 if(!this.data[key]) this.data[key] = [];
                                 this.data[key].push(Object.assign({date: date, value: datapoint[key]},{y:'ppm'}));
                             });
@@ -104,7 +118,7 @@
     };
 </script>
 
-<style>
+<style scoped>
     #timeline {
         border: 4px solid black;
     }
--- src/components/Timeline/Timeline.vue
+++ src/components/Timeline/Timeline.vue
@@ -25,7 +25,7 @@
                 hasLoadedApp: false,
                 devices: [],
                 configs: {},
-                colors: {co2: "grey",temperature: "orange", humidity: "light-blue", light: "yellow", motion: "red"}
+                colors: {co2: "grey",temperature: "orange", humidity: "lightblue", light: "yellow", motion: "red"}
             }
         },
         mounted() {
