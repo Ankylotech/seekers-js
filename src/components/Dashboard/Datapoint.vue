52e59c4482c9c068a9aede9f5e58c7c029b13030
Jonas Rubeck <jaons.rubeck@qaware.de>
2020-07-27 14:52:06 +0200

    refactoring

--- src/components/Dashboard/Datapoint.vue
+++ src/components/Dashboard/Datapoint.vue
@@ -25,20 +25,32 @@
         mounted() {
             if(this.hasConfig){
                 if(this.name === 'co2'){
-                    if(this.config[0] != null &&this.config[0]["device-config"][this.deviceName]) {
-                        if (parseInt(this.value) > this.config[0]["device-config"][this.deviceName]["error-threshold"]) this.isRed = true;
-                        else if (parseInt(this.value) > this.config[0]["device-config"][this.deviceName]["warn-threshold"]) this.isOrange = true;
+                    let index;
+                    for(let i = 0; i < this.config.length; i++){
+                        if(this.config[i].config === "co2"){
+                            index = i;
+                        }
+                    }
+                    if(this.config[index] != null &&this.config[index]["device-config"][this.deviceName]) {
+                        if (parseInt(this.value) > this.config[index]["device-config"][this.deviceName]["error-threshold"]) this.isRed = true;
+                        else if (parseInt(this.value) > this.config[index]["device-config"][this.deviceName]["warn-threshold"]) this.isOrange = true;
                     }else {
                         if (parseInt(this.value) > 2000) this.isRed = true;
                         else if (parseInt(this.value) > 1400) this.isOrange = true;
                     }
                 }else if(this.name === 'temperature'){
-                    if(this.config[1] != null && this.config[1]["device-config"][this.deviceName]) {
-                        if (parseInt(this.value) < this.config[1]["device-config"][this.deviceName]["lower-warning"]) this.isRed = true;
-                        else if (parseInt(this.value) > this.config[1]["device-config"][this.deviceName]["upper-warning"]) this.isRed = true;
+                    let index;
+                    for(let i = 0; i < this.config.length; i++){
+                        if(this.config[i].config === "temperature"){
+                            index = i;
+                        }
+                    }
+                    if(this.config[index] != null && this.config[index]["device-config"][this.deviceName]) {
+                        if (parseFloat(this.value) < this.config[index]["device-config"][this.deviceName]["lower-warning"]) this.isRed = true;
+                        else if (parseFloat(this.value) > this.config[index]["device-config"][this.deviceName]["upper-warning"]) this.isRed = true;
                     }else {
-                        if (parseInt(this.value) < 0) this.isRed = true;
-                        else if (parseInt(this.value) > 30) this.isRed = true;
+                        if (parseFloat(this.value) < 0) this.isRed = true;
+                        else if (parseFloat(this.value) > 30) this.isRed = true;
 
                     }
                 }
