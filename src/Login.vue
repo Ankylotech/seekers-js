34591e0907e3ec38aad752d519bbbcc4a4d84feb
Jonas Rubeck <jaons.rubeck@qaware.de>
2020-07-31 11:08:46 +0200

    Styles

--- src/Login.vue
+++ src/Login.vue
@@ -7,17 +7,33 @@
              dense
              color="#28537D"
              dark
-             src="https://www.qaware.de/fileadmin/user_upload/Stagemoods/qaw-stage-home.png">
+             shrink-on-scroll
+             scroll-target="#scrolling"
+             src="https://www.qaware.de/fileadmin/user_upload/Stagemoods/qaw-stage-home.png"
+             fade-img-on-scroll>
         <v-row justify="space-around">
+            <v-spacer/>
+            <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
             <v-card-title class="white--text"> Please login to your QAware Google Account </v-card-title>
+          <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
+          <v-spacer/>
+          <v-card color="black" max-height="40">
+            <GoogleLogin class="white--text px-4" color="black" :params="params" :onSuccess="onSuccess" :onFailure="onFailure">Login</GoogleLogin>
+          </v-card>
+
         </v-row>
 
   </v-app-bar>
   <v-main >
     <v-layout align-center justify-center>
-    <v-card color="black">
-      <GoogleLogin class="white--text px-4" color="black" :params="params" :onSuccess="onSuccess" :onFailure="onFailure">Login</GoogleLogin>
-    </v-card>
+
     </v-layout>
   </v-main>
   </div>
